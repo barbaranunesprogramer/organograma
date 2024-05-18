649840716099032c0b58d474599eb7149fde43be
Bárbara Elen <barbaranunesprogramer@hotmail.com>
2024-05-18 12:01:32 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX parece html mais não e 
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página do organograma" />
+        </header>
+    )
+
+}
+
+
+export default Banner
